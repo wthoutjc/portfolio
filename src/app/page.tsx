9a7abe0af3f86d66524a791d60f99c1f2db98415
wthoutjc <juancamilorr2012@hotmail.com>
2023-08-28 21:03:44 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,5 +6,7 @@ import { getProjects } from "@/services";
 
 export default async function Home() {
   const { projects } = await getProjects();
+
+  if (projects.length === 0) return <h1>No projects found</h1>;
   return <ProjectsList projects={projects} />;
 }
